e8a85c697b5020e5d5c5962e6e3a2b03bf301b80
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-02-25 22:24:18 +0300

    3.  Madde Yapıldı.

--- Odev4.sql
+++ Odev4.sql
@@ -10,3 +10,12 @@ birbirinden farklı kaç tane veri vardır?
 */
 SELECT COUNT(DISTINCT(replacement_cost) )
 FROM film;
+/*
+3)film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar
+ve aynı zamanda rating 'G' ye eşittir?
+*/
+SELECT COUNT(DISTINCT(title) )
+FROM film
+WHERE title
+LIKE 'T%'
+AND rating = 'G';
