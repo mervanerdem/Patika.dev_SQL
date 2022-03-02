5b103be11d56e19c57d09d0ce1daa921bd42689a
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-02 17:47:48 +0300

    3.ödev yapıldı.

--- Odev6.sql
+++ Odev6.sql
@@ -9,3 +9,10 @@ FROM film;
 SELECT COUNT(title)
 FROM film
 WHERE title LIKE 'C%';
+/*
+3)film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit 
+olan en uzun (length) film kaç dakikadır?
+*/
+SELECT MAX(length)
+FROM film
+WHERE rental_rate = 0.99;
