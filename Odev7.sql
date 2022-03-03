0488d9ed9454cd96ca7d442885927d9d940dd6cc
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-03 16:16:44 +0300

    Odev7 Eklendi.

--- Odev7.sql
+++ Odev7.sql
@@ -19,3 +19,12 @@ HAVING COUNT(replacement_cost) > 50 ;
 SELECT store_id, COUNT(customer)
 FROM customer
 GROUP BY store_id;
+/*
+4) city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra 
+en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+*/
+SELECT MAX(country_id),COUNT(city)
+FROM city
+GROUP BY country_id
+ORDER BY COUNT(city) DESC
+LIMIT 1;
