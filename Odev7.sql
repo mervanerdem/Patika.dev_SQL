6a8be2912ed03d2b6b5547efe47b15e1bf68d7a7
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-03 16:03:55 +0300

    3.madde yapıldı.

--- Odev7.sql
+++ Odev7.sql
@@ -5,3 +5,17 @@ SELECT DISTINCT(rating),title
 FROM film
 GROUP BY title,rating
 ORDER BY rating;
+/*
+2)film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda 
+film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+*/
+SELECT replacement_cost, COUNT(replacement_cost)
+FROM film
+GROUP BY replacement_cost
+HAVING COUNT(replacement_cost) > 50 ;
+/*
+3)customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? 
+*/
+SELECT store_id, COUNT(customer)
+FROM customer
+GROUP BY store_id;
