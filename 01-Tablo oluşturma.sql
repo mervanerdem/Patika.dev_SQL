662ea79956027afa0080044fde69e72cf0a25110
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-15 17:08:58 +0300

    Tablo oluşturulduğu doğrulandı.

--- 01-Tablo oluşturma.sql
+++ 01-Tablo oluşturma.sql
@@ -1,8 +1,10 @@
-CREATE TABLE  author ( --oluşturacağımız tablo ismi 
+/* CREATE TABLE  author ( --oluşturacağımız tablo ismi 
 	id SERIAL PRIMARY KEY,  --id isimli sutün ve sıralı olarak artan veri tipi 
 	first_name  VARCHAR(50) NOT NULL,   --ismi 50 karakter olabilir(string) ve boş bırakılamaz
 	last_name  VARCHAR(50) NOT NULL,
 	email VARCHAR(100), --50 karaktere kadar olabilecek email adresi boş olabilir.
 	birthday DATE   --doğum tarihi veri tipi TARİH ve boş olabilir.
 	
-);
+); */
+
+SELECT * FROM author; --Tablo geldiğini kontrol ediyoruz.
