21c990518838a8ec202b380a5deea75918bd568d
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-17 14:02:33 +0300

    Tablodaki verileri silme

--- 03-Veri-Güncelleme-Silme.sql
+++ 03-Veri-Güncelleme-Silme.sql
@@ -5,8 +5,12 @@ SET first_name = 'Defne'    -- Sutun ve değeri
     birthday = '1919-01-01'
 WHERE id = 10;  --WHERE ile istediğimiz sutunu değiştiririz. Aksi takdirde Tablodaki tüm değerler yukarıdaki gibi güncellenir.
  */
-
+/* 
 UPDATE author   
 SET		last_name = 'Update'    --Belirlenen satırın sadece last_name sütununu güncelle
 WHERE first_name = 'Barnie' --Sadece ilk ismi Barnie olan satırı değiştir
 RETURNING *;    --Yapılan değişikliğin tamamını (* sayesinde) göster.
+ */
+ 
+DELETE FROM author	--sileceğimiz tabloyu seçiyoruz.
+WHERE id = '7';	--Tablonun neresinden
