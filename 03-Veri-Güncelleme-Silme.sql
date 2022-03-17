1a4548a5118d4fd6f10c4fcb4d61384097b5538e
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-17 13:59:11 +0300

    Tablodaki veri parçacıklarını güncelleme

--- 03-Veri-Güncelleme-Silme.sql
+++ 03-Veri-Güncelleme-Silme.sql
@@ -1,6 +1,12 @@
-UPDATE author   --Güncellenecek tablo adı
+/* UPDATE author   --Güncellenecek tablo adı
 SET first_name = 'Defne'    -- Sutun ve değeri 
     last_name = 'Polat'
     email = 'defne@polat.com'
     birthday = '1919-01-01'
 WHERE id = 10;  --WHERE ile istediğimiz sutunu değiştiririz. Aksi takdirde Tablodaki tüm değerler yukarıdaki gibi güncellenir.
+ */
+
+UPDATE author   
+SET		last_name = 'Update'    --Belirlenen satırın sadece last_name sütununu güncelle
+WHERE first_name = 'Barnie' --Sadece ilk ismi Barnie olan satırı değiştir
+RETURNING *;    --Yapılan değişikliğin tamamını (* sayesinde) göster.
